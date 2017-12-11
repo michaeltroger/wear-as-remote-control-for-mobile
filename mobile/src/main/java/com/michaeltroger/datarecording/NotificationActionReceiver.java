d155cafc09e355eac55cd0107fd11d28444bc79e
Michael Troger <dev@michaeltroger.com>
2017-12-11 17:06:35 +0100

    Add log output

--- mobile/src/main/java/com/michaeltroger/datarecording/NotificationActionReceiver.java
+++ mobile/src/main/java/com/michaeltroger/datarecording/NotificationActionReceiver.java
@@ -4,14 +4,17 @@ package com.michaeltroger.datarecording;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class NotificationActionReceiver extends BroadcastReceiver {
     public static final String NOTIFICATION_ACTION = "command";
     public static final String NOTIFICATION_STOP_COMMAND = "stop";
+    private static final String TAG = NotificationActionReceiver.class.getSimpleName();
 
     @Override
     public void onReceive(Context context, Intent intent) {
         final String command = intent.getStringExtra(NOTIFICATION_ACTION);
+        Log.d(TAG, "Received command "+ command +" from notification action");
 
         switch (command) {
             case NOTIFICATION_STOP_COMMAND:
