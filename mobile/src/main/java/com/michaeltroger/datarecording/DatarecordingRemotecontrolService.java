3604a37e92ab8c80bc389b0d40ef1b22d6dbfbcc
Michael Troger <dev@michaeltroger.com>
2017-12-11 15:57:38 +0100

    Remove unneeded google api client from mobile

--- mobile/src/main/java/com/michaeltroger/datarecording/DatarecordingRemotecontrolService.java
+++ mobile/src/main/java/com/michaeltroger/datarecording/DatarecordingRemotecontrolService.java
@@ -20,59 +20,6 @@ public class DatarecordingRemotecontrolService extends WearableListenerService {
 
     private static final String TAG = DatarecordingRemotecontrolService.class.getSimpleName();
 
-    private GoogleApiClient googleApiClient;
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(TAG, "DatarecordingRemotecontrolService");
-
-        startInForeground();
-
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Wearable.API)
-                .build();
-        googleApiClient.connect();
-
-        return START_NOT_STICKY;
-    }
-
-    private void startInForeground() {
-        final NotificationManager service = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        String channelId = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            channelId = createNotificationChannel();
-        }
-
-        final NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId);
-        final Notification notification = notificationBuilder
-                .setOngoing(true)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setContentTitle("remote control")
-                .setCategory(Notification.CATEGORY_SERVICE)
-                .build();
-
-        startForeground(102, notification);
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private String createNotificationChannel() {
-        String channelId = "my_service";
-        String channelName = "My Background Service";
-        NotificationChannel chan = new NotificationChannel(
-                channelId,
-                channelName,
-                NotificationManager.IMPORTANCE_HIGH
-        );
-        chan.setLightColor(Color.BLUE);
-        chan.setImportance(NotificationManager.IMPORTANCE_NONE);
-        chan.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
-        final NotificationManager service = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        service.createNotificationChannel(chan);
-        return channelId;
-    }
-
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
         final String path = messageEvent.getPath();
